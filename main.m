afc742494f735c41b8bec9529f7500b319063979
kobiRasing <kobirasing7@gmail.com>
2023-11-29 15:03:33 +0800

    added folder for license plate characters in philippine font

--- main.m
+++ main.m
@@ -3,44 +3,44 @@
 
 %% Template Letters (To be compared with the input)
 % Alphabets
-A=imread('alpha/A.bmp');
-B=imread('alpha/B.bmp');
-C=imread('alpha/C.bmp');
-D=imread('alpha/D.bmp');
-E=imread('alpha/E.bmp');
-F=imread('alpha/F.bmp');
-G=imread('alpha/G.bmp');
-H=imread('alpha/H.bmp');
-I=imread('alpha/I.bmp');
-J=imread('alpha/J.bmp');
-K=imread('alpha/K.bmp');
-L=imread('alpha/L.bmp');
-M=imread('alpha/M.bmp');
-N=imread('alpha/N.bmp');
-O=imread('alpha/O.bmp');
-P=imread('alpha/P.bmp');
-Q=imread('alpha/Q.bmp');
-R=imread('alpha/R.bmp');
-S=imread('alpha/S.bmp');
-T=imread('alpha/T.bmp');
-U=imread('alpha/U.bmp');
-V=imread('alpha/V.bmp');
-W=imread('alpha/W.bmp');
-X=imread('alpha/X.bmp');
-Y=imread('alpha/Y.bmp');
-Z=imread('alpha/Z.bmp');
+A=imread('phfont/A.jpg');
+B=imread('phfont/B.jpg');
+C=imread('phfont/C.jpg');
+D=imread('phfont/D.jpg');
+E=imread('phfont/E.jpg');
+F=imread('phfont/F.jpg');
+G=imread('phfont/G.jpg');
+H=imread('phfont/H.jpg');
+I=imread('phfont/I.jpg');
+J=imread('phfont/J.jpg');
+K=imread('phfont/K.jpg');
+L=imread('phfont/L.jpg');
+M=imread('phfont/M.jpg');
+N=imread('phfont/N.jpg');
+O=imread('phfont/O.jpg');
+P=imread('phfont/P.jpg');
+Q=imread('phfont/Q.jpg');
+R=imread('phfont/R.jpg');
+S=imread('phfont/S.jpg');
+T=imread('phfont/T.jpg');
+U=imread('phfont/U.jpg');
+V=imread('phfont/V.jpg');
+W=imread('phfont/W.jpg');
+X=imread('phfont/X.jpg');
+Y=imread('phfont/Y.jpg');
+Z=imread('phfont/Z.jpg');
 
 % Natural Numbers
-one=imread('alpha/1.bmp');
-two=imread('alpha/2.bmp');
-three=imread('alpha/3.bmp');
-four=imread('alpha/4.bmp');
-five=imread('alpha/5.bmp');
-six=imread('alpha/6.bmp');
-seven=imread('alpha/7.bmp');
-eight=imread('alpha/8.bmp');
-nine=imread('alpha/9.bmp');
-zero=imread('alpha/0.bmp');
+one=imread('phfont/1.jpg');
+two=imread('phfont/2.jpg');
+three=imread('phfont/3.jpg');
+four=imread('phfont/4.jpg');
+five=imread('phfont/5.jpg');
+six=imread('phfont/6.jpg');
+seven=imread('phfont/7.jpg');
+eight=imread('phfont/8.jpg');
+nine=imread('phfont/9.jpg');
+zero=imread('phfont/0.jpg');
 
 % Creating Array for Alphabets
 letter = {A B C D E F G H I J K L M N O P Q R S T U V W X Y Z};
